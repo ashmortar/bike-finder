461dd5e9432ab99ea6299b46aa832e5444f06d8c
Aaron Ross and Kyle Tenzeldam <aaron@>
2018-02-14 10:28:16 -0800

    initial commit

---
+++ assets/js/app-interface.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#RecipeRequest').click(function() {
+    let recipe = $('#recipe').val();
+  $.ajax({
+    url: https://api2.bigoven.com/recipe/" + RecipeId + "?api_key=" + apiKey;
+    type: "GET";
+    dataType: 
+  })
+  })
+})
