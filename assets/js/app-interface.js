9aa887895b88f5cea62427744728bcd1753b3771
Aaron Ross and Kyle Tenzeldam <aaron@>
2018-02-14 14:19:06 -0800

    add lots of stuff

--- assets/js/app-interface.js
+++ assets/js/app-interface.js
@@ -1,10 +1,29 @@
+import {apiCall} from './../assets/js/app.js';
+const moment = require("moment");
+
 $(document).ready(function() {
-  $('#RecipeRequest').click(function() {
-    let recipe = $('#recipe').val();
-  $.ajax({
-    url: https://api2.bigoven.com/recipe/" + RecipeId + "?api_key=" + apiKey;
-    type: "GET";
-    dataType: 
-  })
-  })
-})
+
+  $('#form').submit(function(event) {
+    event.preventDefault();
+    let location = $('#location').val();
+    let proximity = parseInt($('#proximity').val());
+
+    let newApiCall = apiCall(location, proximity);
+
+    newApiCall.then(function(response) {
+      let body = JSON.parse(response);
+      for (var i = 0; i < body.bikes.length; i++) {
+        let date = new moment(body.bikes[i].date_stolen);
+        let formatted = date.format("dddd MMMM DDDo, YYYY");
+        if(body.bikes[i].thumb != null) {
+          $('#results').append('<div class="list-item"><li><img src="' + body.bikes[i].thumb + '"><ul><li>' + body.bikes[i].title +'</li><li>'+ body.bikes[i].frame_colors +'</li><li>' + body.bikes[i].stolen_location + '</li><li>'+ formatted +'</li></ul></li></div>');
+        } else {
+            $('#results').append('<div class="list-item"><li><img src="./../assets/images/bike-icon.png"><ul><li>' + body.bikes[i].title +'</li><li>'+ body.bikes[i].frame_colors +'</li><li>' + body.bikes[i].stolen_location + '</li><li>'+ formatted +'</li></ul></li></div>');
+        }
+        console.log(body.bikes[i].title);
+      }
+    }, function(error) {
+      console.log(error.message);
+    });
+  });
+});
